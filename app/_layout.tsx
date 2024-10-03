68be4b224b1d87207afce080a646a62354019073
Murali Murugan <murali732000@gmail.com>
2024-10-04 00:52:51 +0100

    commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,23 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Link } from "expo-router";
+import { SafeAreaView, Text } from "react-native";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+export default async function Layout() {
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+    return (
+        <SafeAreaView>
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+            <Link href={"/explore"}>
+                <Text>Explore</Text>
+            </Link>
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+            <Link href={"/foryou"}>
+                <Text>Foryou</Text>
+            </Link>
 
-  if (!loaded) {
-    return null;
-  }
+            <Link href={"/account"}>
+                <Text>account</Text>
+            </Link>
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
+        </SafeAreaView>
+    )
 }
---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
+
+
+export default function () {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+      
+        let interval = setInterval(() => {
+            setCount((c) => c + 1);
+        }
+            , 1000);
+        
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+
+    
+
+    return (
+        <View>
+       
+        </View>
+    )
+}
